a939e1b9b28c8de872b68d4fd521bdd2c1e0b9f0
JerzyJancewicz <jancewiczjerzy@gmail.com>
2023-03-14 16:39:15 +0100

    bu with Z

--- sin/main.py
+++ sin/main.py
@@ -1,50 +1,59 @@
 import math
 import numpy as np
-import matplotlib.pyplot as plt
 
 
 def taylor(x, times):
     taylor_sin = x - ((x ** 3) / math.factorial(3))
-
+    tmp_taylor = taylor_sin
     z = 5
-    for a in range(0, times):
+    for a in range(times):
         if a % 2 == 0:
-            print(a)
-            taylor_sin = taylor_sin - ((x ** z) / math.factorial(z))
-            print(taylor_sin)
+            taylor_sin = tmp_taylor + ((x ** z) / math.factorial(z))
+            #print(str(math.factorial(z)) + " " + str(z))
         else:
-            taylor_sin = taylor_sin + ((x ** z) / math.factorial(z))
+            taylor_sin = tmp_taylor - ((x ** z) / math.factorial(z))
+            #print(str(math.factorial(z)) + " " + str(z))
         z = z + 2
 
     return taylor_sin
 
 
 def check_x(numb):
-
-    if 0 < numb <= np.pi/2:
+    if 0 <= numb < math.pi/2:
         return numb
-    elif np.pi/2 < numb <= np.pi:
-        return np.pi - numb
-    elif np.pi < numb < 3/2*np.pi:
-        return numb - np.pi
-    elif 3/2*np.pi < numb <= 2*np.pi:
-        return 2*np.pi - numb
+    elif math.pi/2 <= numb < math.pi:
+        return math.pi - numb
+    elif math.pi <= numb < 3/2*math.pi:
+        return math.pi - numb
+    elif 3/2*math.pi <= numb < 2*math.pi:
+        return numb - 2*math.pi
     else:
-        print(numb)
         return 0
 
 
-x = np.arange(0, 2*np.pi, 0.1)   # start,stop,step
+first = 0
+for k in range(100):
+    current = first + math.pi / 20
 
-# Call check_x function on each element in x
-y = taylor(np.array([check_x(xi) for xi in x]), 6)
+    taylor_numb = taylor(check_x(current), 50)
+    np_sin = math.sin(current)
+    measurement_error = np_sin - taylor_numb
 
-# Convert output back to degrees
-# y = np.degrees(y)
+    print(str(taylor_numb) + " " + str(np_sin) + " " + str(measurement_error))
+    first = current
 
-# Plot the result
-plt.plot(x, y)
-plt.xlabel('x')
-plt.ylabel('sin(x)')
-plt.title('Taylor Series Approximation of Sin(x)')
-plt.show()
+
+# x = np.arange(0, 2*np.pi, 0.1)   # start,stop,step
+#
+# # Call check_x function on each element in x
+# y = taylor(np.array([check_x(xi) for xi in x]), 6)
+#
+# # Convert output back to degrees
+# # y = np.degrees(y)
+#
+# # Plot the result
+# plt.plot(x, y)
+# plt.xlabel('x')
+# plt.ylabel('sin(x)')
+# plt.title('Taylor Series Approximation of Sin(x)')
+# plt.show()
